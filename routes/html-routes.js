4e9e0fff2b1576692be6601d387cf3a43415ca1a
Steve S. Lee <sjl558@nyu.edu>
2020-06-25 18:37:20 -0400

    html routes update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,19 +14,11 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/userRoutines", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/userRoutines.html"));
   });
-
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  });
+  
 };
