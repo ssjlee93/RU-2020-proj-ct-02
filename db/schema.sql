bb267f1277a4b39e7dba5372f512239938b7e551
Steve S. Lee <sjl558@nyu.edu>
2020-07-01 01:00:41 -0400

    deleted unnecessary files, editied HTML p tags, README update

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,2 @@
 DROP DATABASE IF EXISTS pr_db;
 CREATE DATABASE pr_db;
-
-USE pr_db; 
-
-CREATE TABLE exercizes (
-id INT AUTO_INCREMENT NOT NULL,
-PRIMARY KEY(id),
-weights INT,
-reps INT NOT NULL DEFAULT 10,
-exercise varchar(255) NOT NULL
-);  
-
-CREATE TABLE routines (
-id INT AUTO_INCREMENT NOT NULL,
-PRIMARY KEY(id),
-routine_name varchar(255) NOT NULL, 
-sets INT NOT NULL DEFAULT 1,
-exercise_id INT REFERENCES exercizes(id)
-);  
--- db/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO routines (routine_name) 
-VALUES ("Steve's Routine"), ("Erica's Routine"), ("Kobe's Routine"), ("Jenny's Routine");
-INSERT INTO exercizes (exercize) 
-VALUES ("Push-ups"), ("Pull-ups"), ("Chin-ups"), ("Shut-ups"), ("Roll-ups");
