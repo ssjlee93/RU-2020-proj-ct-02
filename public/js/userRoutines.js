0e82e23ea6c298f72757d4c3d89480798249ebaf
Kobe Matias <kobematias@yahoo.com>
2020-06-30 21:14:57 -0400

    merged html

--- public/js/userRoutines.js
+++ public/js/userRoutines.js
@@ -27,14 +27,39 @@ $(document).ready(function(){
 
     $(".saveRoutine").on("click", function(event) {
         event.preventDefault();
-        var routineName = $("#routineName").val().trim();
-        var sets = parseInt($("#sets").val());
-        var exercise1 = $("#exercise1").val().trim();
-        var repCount1 = parseInt($("#repCount1").val());
-        var exercise2 = $("#exercise2").val().trim();
-        var repCount2 = parseInt($("#repCount2").val());
-        var exercise3 = $("#exercise3").val().trim();
-        var repCount3 = parseInt($("#repCount3").val());
+        $("#myRoutine").empty();
+        var routineName = $("#editroutineName").val().trim();
+        var sets = parseInt($("#editsets").val());
+        var exercise1 = $("#editexercise1").val().trim();
+        var repCount1 = parseInt($("#editrepCount1").val());
+        var exercise2 = $("#editexercise2").val().trim();
+        var repCount2 = parseInt($("#editrepCount2").val());
+        var exercise3 = $("#editexercise3").val().trim();
+        var repCount3 = parseInt($("#editrepCount3").val());
+        
+        if (!routineName || !sets  || !exercise1  || !exercise2  || !exercise3  || !repCount1  || !repCount2  || !repCount3 ) {
+            var myRoutine = 
+                `<div class="modal-content center-align">
+                        <h2>Please Fill in all fields</h2>
+                    </div> 
+                    <div class="modal-footer">
+                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+                    </div>` 
+            $("#myRoutine").append(myRoutine);
+            } else {
+            var myRoutine = 
+                    ` <div class="modal-content center-align">
+                                <p>Your routine name is <b>${routineName}</b> and it has <b>${sets}</b> sets. </p>
+                                    <p>Your first exercise is <b>${repCount1}</b> <b>${exercise1}</b></p>
+                                    <p>Your second exercise is <b>${repCount2}</b> <b>${exercise2}</b></p>
+                                    <p>Your third exercise is <b>${repCount3}</b> <b>${exercise3}</b></p>
+                                </div> 
+                                <div class="modal-footer">
+                                    <a href="/userRoutines" class="waves-effect waves-green btn">Lets view your routine!</a>
+                                </div>` 
+            $("#myRoutine").append(myRoutine);           
+            }
+        
         let editRoutine = {
             routineName: routineName,
             sets: sets,
